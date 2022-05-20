a097f36697fb54d1fa87f323484a1ff4460424b8
Vasilii <84954523+V-Ivanich@users.noreply.github.com>
2022-05-20 11:35:59 +0300

    all ready

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './source/Header';
 import Header from './source/Header';
 import './source/Main';
 import Main from './source/Main';
+import Footer from './source/Footer';
 
 class App extends React.Component {
   render() {
@@ -12,6 +13,7 @@ class App extends React.Component {
       <>
         <Header />
         <Main />
+        <Footer />
       </>
     );
   }
---
+++ src/source/Footer.jsx
@@ -0,0 +1,22 @@
+
+import React from 'react';
+import './Footer.css';
+
+class Footer extends React.Component {
+  render() {
+  return (
+    <>
+    <footer className="foo">
+    <h4>
+      Учебный/тестовый проектик.
+    </h4>
+    <p>
+      Изучение React-а начато в начале мая 2022 года.
+    </p>
+    </footer>
+    </>
+  );
+}
+}
+
+export default Footer;
